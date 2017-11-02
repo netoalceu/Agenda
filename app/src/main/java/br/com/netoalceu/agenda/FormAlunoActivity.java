8d1ae015a0f365de4f313ebb4660cf39e07a85d8
Alceu Rosa Neto <neto.alceu@gmail.com>
2017-11-02 20:41:02 -0200

    Alura - Android I - Aula 2 - Agenda - Finalizando a criação do Formulario de cadastro do aluno, Criação do lisnen do botao salvar e mostra um Toast

--- app/src/main/java/br/com/netoalceu/agenda/FormAlunoActivity.java
+++ app/src/main/java/br/com/netoalceu/agenda/FormAlunoActivity.java
@@ -2,6 +2,9 @@ package br.com.netoalceu.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormAlunoActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class FormAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_aluno);
+
+        Button btnSalvar = (Button) findViewById(R.id.FormAlunoBtnSalvar);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormAlunoActivity.this,"Botao Clicado",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
