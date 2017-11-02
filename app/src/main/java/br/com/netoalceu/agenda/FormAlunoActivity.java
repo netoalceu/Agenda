f007fd30be2e61012e971c3f3c4d3889343b6c73
Alceu Rosa Neto <neto.alceu@gmail.com>
2017-11-02 19:22:35 -0200

    Alura - Android I - Aula 2 - Agenda - Finalizando a criação do Formulario de cadastro do aluno

---
+++ app/src/main/java/br/com/netoalceu/agenda/FormAlunoActivity.java
@@ -0,0 +1,13 @@
+package br.com.netoalceu.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_aluno);
+    }
+}
